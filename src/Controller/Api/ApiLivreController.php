54d6ad18809b9da8ad088f7dd15bd43334276add
Jules-95 <jlsmillet@yahoo.fr>
2026-05-20 14:02:58 +0200

    Ajout endpoint POST api/livres

--- src/Controller/Api/ApiLivreController.php
+++ src/Controller/Api/ApiLivreController.php
@@ -5,8 +5,10 @@ namespace App\Controller\Api;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Livre;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/api/livres')]
 final class ApiLivreController extends AbstractController
@@ -28,6 +30,7 @@ final class ApiLivreController extends AbstractController
         }
         
         return $this->json($data);
+        // TEST POSTMAN OK
     }
 
     #[Route('/{id}', name: 'api_livre_show', methods: ['GET'])]
@@ -40,7 +43,31 @@ final class ApiLivreController extends AbstractController
         'isbn' => $livre->getIsbn(),
         'disponible' => $livre->isDisponible(),
         ]);
+        // TEST POSTMAN OK
     }
+
+    // Methode POST
+
+    #[Route('', name: 'api_livre_new', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        // Je passe le JSON en tableau PHHP pour pouvoir utiliser $data['...']
+    $data = json_decode($request->getContent(), true);
+
+    $livre = new Livre();
+    $livre->setTitre($data['titre']);
+    $livre->setAuteur($data['auteur']);
+    $livre->setIsbn($data['isbn']);
+    $livre->setDatePublication(new \DateTimeImmutable($data['datePublication']));
+    $livre->setDisponible(true);
+
+    // entity Manager ->Equivalent ici pdo prepare et $stmt execute en php natif
+    $em->persist($livre);
+    $em->flush();
+
+    return $this->json(['message' => 'Livre ajouté !', 'id' => $livre->getId()], 201);
+    // TEST POSMAN OK
+}
 }
 
 
